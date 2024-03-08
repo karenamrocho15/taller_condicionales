a6ad574fdc5e09744be70360e459cdf3517bdd00
sistemas <karenamorocho@colegioguanenta.edu.co>
2024-03-08 08:49:17 -0500

    se agrego el codigo python del programa

--- ejercicio-1/ejercicio_1.py
+++ ejercicio-1/ejercicio_1.py
@@ -1,17 +1,19 @@
 #programa para calcular en que cuadrante se encuentra un punto en el plano carteciano
 
-#entrada 
-X= int(input("ingresa la cordenada x: "))
-Y = int(input("ingresa la cordenada Y: "))
+#input
+print("-------------------------------------------------")
 
-#proceso
+X= int(input("ingresa la cordenada x en el plano: "))
+Y = int(input("ingresa la cordenada Y en el plano: "))
+
+#processing
 if  X == 0:
     if Y == 0:
-        print("la coordenada"(X , Y), "esta en el origen")
+        print("la coordenada", (X , Y), "esta en el origen")
     else:
-         print("la coordenada"(X , Y), "esta en el eje Y")
+         print("la coordenada", (X , Y), "esta en el eje Y")
 elif Y == 0:
-      print("la coordenada"(X , Y), "esta en el eje X")
+      print("la coordenada", (X , Y), "esta en el eje X")
 elif X > 0:
     if Y > 0: 
         print( "la coordenada"(X , Y), "esta en el el cuadrante 1") 
